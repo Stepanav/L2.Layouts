a6f0143c967daa0b52ba775d857d4600f8f7669d
Stepanav <vsa83@yandex.ru>
2021-12-26 13:07:58 +0300

    create second Activity

--- app/src/main/java/ru/gb/course1/l2layouts/MainActivity.java
+++ app/src/main/java/ru/gb/course1/l2layouts/MainActivity.java
@@ -2,13 +2,32 @@ package ru.gb.course1.l2layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnActTwo;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener((View.OnClickListener) this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                Intent intent = new Intent(this, SecondActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
